1006c4200bfd7cd9c0e2c6d5f32e1b76a178a273
mariacanovfandos <mariacf2003@gmail.com>
2026-05-20 21:34:00 +0200

    Update and rename CTD_diseases_pathways2.R to CTD_Diseases_Pathways.R

--- CODE./CTD_Diseases_Pathways.R
+++ CODE./CTD_Diseases_Pathways.R
@@ -24,7 +24,7 @@ datos_enfermedad_ruta <- datos_enfermedad_ruta %>%
 # ----------- SERIALIZACIÓN ------------
 
 # 1. Guardar grafo
-ruta_guardado_asociaciones <- "../RESULTADOS/diseases_pathways_association_ctd.ttl"
+ruta_guardado_asociaciones <- "../RESULTADOS/CTD_Diseases_Pathways_association.ttl"
 cat("", file = ruta_guardado_asociaciones)
 
 # 2. Prefijos
